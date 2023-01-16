81981a37a9d28f73d7c447fc5e3dd1d0db614b46
KipoGo <vit-tan@mail.ru>
2023-01-16 18:13:32 +0300

    Add
    Added new class

---
+++ Assets/SlimeEvolutions/Scripts/Panel/Crossing/UpdateView/UpdateViewBehaviour/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.SlimeEvolutions.Scripts.Panel.Crossing.UpdateView.UpdateViewBehaviour
+{
+    class Class1
+    {
+    }
+}
