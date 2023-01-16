37619548292eaddcb8e6a25a0255ce87e40629d8
KipoGo <vit-tan@mail.ru>
2023-01-16 15:18:53 +0300

    Add feature
    added new class

---
+++ Assets/SlimeEvolutions/Scripts/Panel/Crossing/UpdateView.cs
@@ -0,0 +1,34 @@
+﻿using SlimeEvolutions.Architecture.CrossData;
+using SlimeEvolutions.Architecture.Interactors.Instances;
+using SlimeEvolutions.Architecture.Scene;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SlimeEvolutions.Panel.Crossing
+{
+    public class UpdateView
+    {
+        private CrossingSpaceData crossingSpaceData;
+
+
+        public UpdateView()
+        {
+
+        }
+
+
+        private void Initialize(int id)
+        {
+            crossingSpaceData = Game.GetInteractor<CrossingSpaceInteractor>().CrossingSpaces[id];
+        }
+
+        private void Upadate()
+        {
+
+        }
+    }
+}
