b1e16343383a5824f93de56959a84fb06aa21531
KipoGo <vit-tan@mail.ru>
2023-01-16 18:21:28 +0300

    Add
    Added new class

---
+++ Assets/SlimeEvolutions/Scripts/Panel/Crossing/UpdateView/UpdateViewBehaviour/TimeIsNotAvailableBehaviour.cs
@@ -0,0 +1,47 @@
+﻿namespace SlimeEvolutions.Panel.Crossing.Update.Behaviours
+{
+    public class TimeIsNotAvailableBehaviour : IUpdateViewBehaviour
+    {
+        private UpdateView updateView;
+        private bool isSubscribe;
+
+        public void Enter(UpdateView updateView)
+        {
+            this.updateView = updateView;
+            Subscribe();
+        }
+
+        public void Exit()
+        {
+            Unsubscribe();
+        }
+
+        public void OnEnable()
+        {
+            Subscribe();
+        }
+
+        public void OnDisable()
+        {
+            Unsubscribe();
+        }
+
+        private void Subscribe()
+        {
+            if (isSubscribe)
+            {
+                return;
+            }
+            isSubscribe = true;
+        }
+
+        private void Unsubscribe()
+        {
+            if (!isSubscribe)
+            {
+                return;
+            }
+            isSubscribe = false;
+        }
+    }
+}
