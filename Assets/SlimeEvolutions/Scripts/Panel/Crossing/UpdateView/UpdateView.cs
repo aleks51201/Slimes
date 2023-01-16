634dabfeb13cc23ae3f18f7da608d672cb986799
KipoGo <vit-tan@mail.ru>
2023-01-16 17:31:50 +0300

    Add
    Added new property and empty method

--- Assets/SlimeEvolutions/Scripts/Panel/Crossing/UpdateView/UpdateView.cs
+++ Assets/SlimeEvolutions/Scripts/Panel/Crossing/UpdateView/UpdateView.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace SlimeEvolutions.Panel.Crossing
+namespace SlimeEvolutions.Panel.Crossing.Update
 {
     public class UpdateView: IActivatable
     {
