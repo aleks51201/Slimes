c5b86f7804b098e35a1067a73c34cbff81c6042f
KipoGo <vit-tan@mail.ru>
2023-01-16 16:32:19 +0300

    Add feature
    Added new class

---
+++ Assets/SlimeEvolutions/Scripts/Panel/Crossing/UpdateView/UpdateViewBehaviour/ThirdBehaviour.cs
@@ -0,0 +1,47 @@
+﻿namespace SlimeEvolutions.Panel.Crossing.Update.Behaviours
+{
+    public class ThirdBehaviour : IUpdateViewBehaviour
+    {
+        private LaboratoryLogic labLogic;
+        private bool isSubscribe;
+
+        public void Enter(LaboratoryLogic laboratoryLogic)
+        {
+            labLogic = laboratoryLogic;
+            Subscribe();
+        }
+
+        public void Exit()
+        {
+            Unsubscribe();
+        }
+
+        public void OnEnable()
+        {
+            Subscribe();
+        }
+
+        public void OnDisable()
+        {
+            Unsubscribe();
+        }
+
+        private void Subscribe()
+        {
+            if (isSubscribe)
+            {
+                return;
+            }
+            isSubscribe = true;
+        }
+
+        private void Unsubscribe()
+        {
+            if (!isSubscribe)
+            {
+                return;
+            }
+            isSubscribe = false;
+        }
+    }
+}
