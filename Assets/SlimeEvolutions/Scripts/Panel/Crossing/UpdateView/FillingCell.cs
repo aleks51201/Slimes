f383942b73cd4816e76bee757d26ac24da0b18d9
KipoGo <vit-tan@mail.ru>
2023-01-17 17:23:38 +0300

    Add
    Added new classes

---
+++ Assets/SlimeEvolutions/Scripts/Panel/Crossing/UpdateView/FillingCell.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SlimeEvolutions.Panel.Crossing.UpdateView
+{
+    public class FillingCell
+    {
+        private Slime lSlime;
+        private Slime rSlime;
+        private Slime centralSlime;
+
+
+        public FillingCell(Slime lSlime, Slime rSlime, Slime centralSlime)
+        {
+            this.lSlime = lSlime;
+            this.rSlime = rSlime;
+            this.centralSlime = centralSlime;
+        }
+    }
+}
---
+++ Assets/SlimeEvolutions/Scripts/Spawner/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace SlimeEvolutions.Spawner
+{
+    public class Spawner : MonoBehaviour
+    {
+        private static Spawner s_instance;
+
+
+        public static Spawner Instance
+        {
+            get
+            {
+                if (s_instance)
+                {
+                    var go = new GameObject("[SPAWNER");
+                    s_instance = go.AddComponent<Spawner>();
+                    DontDestroyOnLoad(go);
+                }
+                return s_instance;
+            }
+        }
+
+
+        public GameObject SpawnObject(GameObject prefab, Transform parentTransform)
+        {
+            return Instantiate(prefab, parentTransform);
+        }
+    }
+}
