d78c4d5851ba01ec09e44e3965057affead80992
KipoGo <vit-tan@mail.ru>
2023-01-18 15:53:34 +0300

    Add

--- Assets/SlimeEvolutions/Scripts/Panel/Crossing/Timer/TimerView.cs
+++ Assets/SlimeEvolutions/Scripts/Panel/Crossing/Timer/TimerView.cs
@@ -4,27 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using TMPro;
 
-namespace SlimeEvolutions.Panel.Crossing.Timer
+namespace SlimeEvolutions.Panel.Crossing.CrossTimer
 {
     public class TimerView: MonoBehaviour
     {
+        [SerializeField] private TextMeshProUGUI timerText;
+        [SerializeField] private int id;
+
         private TimerLogic timerLogic;
 
 
+        public TextMeshProUGUI TimerText => timerText;
+        public int ID => id;
+
+        
         private void Awake()
         {
-            timerLogic = new();
+            timerLogic = new(this);
         }
 
         private void OnEnable()
         {
-            
+            timerLogic.OnEnable();
         }
 
         private void OnDisable()
         {
-            
+            timerLogic.OnDisable();
         }
     }
 }
