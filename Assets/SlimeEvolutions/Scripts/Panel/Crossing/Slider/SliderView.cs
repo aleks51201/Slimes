1ebd8eba5b9c95aa582476de1d1cd4742d521c9b
KipoGo <vit-tan@mail.ru>
2023-01-18 20:36:14 +0300

    Add

---
+++ Assets/SlimeEvolutions/Scripts/Panel/Crossing/Slider/SliderView.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace SlimeEvolutions.Panel.Crossing.CrossSlider
+{
+    public class SliderView : MonoBehaviour
+    {
+        private SliderLogic sliderLogic;
+
+
+        private void Awake()
+        {
+            sliderLogic = new(this);
+        }
+
+        private void OnEnable()
+        {
+            sliderLogic.OnEnable();
+        }
+
+        private void OnDisable()
+        {
+            sliderLogic.OnDisable();
+        }
+    }
+}
