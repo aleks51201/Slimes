20130d0f6702d201eee046cb96b2cffdec8979ff
KipoGo <vit-tan@mail.ru>
2023-01-18 15:41:37 +0300

    Add
    Added new classes

---
+++ Assets/SlimeEvolutions/Scripts/Panel/Crossing/Timer/TimerLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlimeEvolutions.Panel.Crossing.Timer
+{
+    public class TimerLogic : IActivatable
+    {
+
+        public void OnEnable()
+        {
+        }
+
+        public void OnDisable()
+        {
+        }
+    }
+}
---
+++ Assets/SlimeEvolutions/Scripts/Panel/Crossing/Timer/TimerView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SlimeEvolutions.Panel.Crossing.Timer
+{
+    public class TimerView: MonoBehaviour
+    {
+        private TimerLogic timerLogic;
+
+
+        private void Awake()
+        {
+            timerLogic = new();
+        }
+
+        private void OnEnable()
+        {
+            
+        }
+
+        private void OnDisable()
+        {
+            
+        }
+    }
+}
