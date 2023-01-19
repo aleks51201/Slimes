87e6a5d1b44ff20d6bec16cc13972f279a55c9d0
KipoGo <vit-tan@mail.ru>
2023-01-19 13:21:01 +0300

    Refactoring

--- Assets/SlimeEvolutions/Scripts/Panel/Crossing/Slider/SliderLogic.cs
+++ Assets/SlimeEvolutions/Scripts/Panel/Crossing/Slider/SliderLogic.cs
@@ -13,9 +13,7 @@ namespace SlimeEvolutions.Panel.Crossing.CrossSlider
     public class SliderLogic : IActivatable
     {
         private SliderView sliderView;
-        private Timer timer;
-        private float num;
-        private bool flag;
+        private Coroutine currentCoroutine;
 
 
         private int ID => sliderView.GetComponentInParent<HoldCrossingPlaceView>().ID;
@@ -28,29 +26,6 @@ namespace SlimeEvolutions.Panel.Crossing.CrossSlider
         }
 
 
-        private void StartFillingIn()
-        {
-            float seconds = (float)(CrossingSpaceData.EndTimeCrossing - DateTime.Now).TotalSeconds;
-            timer = new Timer(TimerTypes.OneSecTick, seconds);
-            timer.Start();
-            TimerSubscribe();
-        }
-
-        float seconds;
-        private void UpdateSliderView(float secons)
-        {
-            var slider = sliderView.GetComponent<Slider>();
-            //Debug.Log(secons+"sec") ;
-            //Debug.Log(Mathf.Lerp(0, 1,Time.deltaTime * secons)+"lerp");
-            num += Time.deltaTime;
-            if (flag)
-            {
-                seconds = secons;
-            }
-
-            slider.value = Mathf.Lerp(0, 1, num / seconds);
-        }
-
         private IEnumerator FillValue(float value)
         {
             var estimateTime = 0f;
@@ -60,33 +35,19 @@ namespace SlimeEvolutions.Panel.Crossing.CrossSlider
             while (estimateTime < sec)
             {
                 estimateTime += Time.deltaTime;
-                Debug.Log(estimateTime);
                 slider.value = Mathf.Lerp(0, value, estimateTime / sec);
                 yield return null;
             }
         }
 
-        private void TimerSubscribe()
-        {
-            timer.OnTimerValueChangedEvent += UpdateSliderView;
-        }
-
-        private void TimerUnsubscribe()
-        {
-            timer.OnTimerValueChangedEvent -= UpdateSliderView;
-        }
-
         public void OnEnable()
         {
-            num = 0;
-            flag = true;
-            seconds = 0;
-
-            Coroutines.StartRoutine(FillValue(1));
+            currentCoroutine = Coroutines.StartRoutine(FillValue(1));
         }
 
         public void OnDisable()
         {
+            Coroutines.StopRoutine(currentCoroutine);
         }
     }
 }
