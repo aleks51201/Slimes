69ae5110fe7a5bb020011d230185d459ab7e4863
KipoGo <vit-tan@mail.ru>
2023-01-21 23:27:42 +0300

    Add
    Added new class

---
+++ Assets/SlimeEvolutions/Scripts/Inventory/InventoryButton/SlimeGetter.cs
@@ -0,0 +1,40 @@
+﻿using SlimeEvolutions.Buttons;
+using SlimeEvolutions.InventoryCell;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SlimeEvolutions.Inventory.InventoryButton
+{
+    public class SlimeGetter:MonoBehaviour
+    {
+        [SerializeField] private ButtonWithClickAndHold button;
+
+
+        public static Action<Slime> ButtonClickedEvent;
+
+
+        private Slime GetSlime()
+        {
+            return GetComponentInChildren<CellView>().Slime;
+        }
+
+        private void OnHold()
+        {
+            ButtonClickedEvent?.Invoke(GetSlime());
+        }
+
+        private void OnEnable()
+        {
+            button.OnButtonClickEvent+= OnHold;
+        }
+
+        private void OnDisable()
+        {
+            button.OnButtonClickEvent-= OnHold;
+        }
+    }
+}
