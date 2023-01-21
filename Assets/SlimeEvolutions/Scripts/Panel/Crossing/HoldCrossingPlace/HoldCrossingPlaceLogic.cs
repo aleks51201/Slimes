7a64062cd2599bab84f6f68d4315900359bd62de
KipoGo <vit-tan@mail.ru>
2023-01-21 21:08:45 +0300

    Refactoring

--- Assets/SlimeEvolutions/Scripts/Panel/Crossing/HoldCrossingPlace/HoldCrossingPlaceLogic.cs
+++ Assets/SlimeEvolutions/Scripts/Panel/Crossing/HoldCrossingPlace/HoldCrossingPlaceLogic.cs
@@ -1,10 +1,7 @@
 ﻿using SlimeEvolutions.Architecture.CrossData;
 using SlimeEvolutions.Architecture.Interactors.Instances;
 using SlimeEvolutions.Architecture.Scene;
-using SlimeEvolutions.Buttons;
-using SlimeEvolutions.InventoryCell;
 using SlimeEvolutions.Panel.Crossing.Update;
-using TMPro;
 
 namespace SlimeEvolutions.Panel.Crossing
 {
@@ -36,29 +33,6 @@ namespace SlimeEvolutions.Panel.Crossing
             updateView.Initialize(holdCrossingPlaceView.ID);
         }
 
-        private void DeleteCellFromButton(ButtonMain btn)
-        {
-            try
-            {
-                holdCrossingPlaceView.Delete(btn.GetComponentInChildren<CellView>().gameObject);
-            }
-            catch
-            {
-                return;
-            }
-        }
-
-        private void Clean()
-        {
-            var btns = new ButtonMain[] { holdCrossingPlaceView.LeftSlime, holdCrossingPlaceView.RightSlime, holdCrossingPlaceView.AcceptButton };
-            foreach (ButtonMain btn in btns)
-            {
-                DeleteCellFromButton(btn);
-                //SetButtonStatus(btn, false);
-            }
-            holdCrossingPlaceView.Timer.GetComponentInChildren<TextMeshProUGUI>().text = "";
-        }
-
         private void SaveSlime(Slime[] slimes)
         {
             foreach (Slime slime in slimes)
@@ -77,13 +51,7 @@ namespace SlimeEvolutions.Panel.Crossing
             };
             SaveSlime(slimes);
             Game.GetInteractor<CrossingSpaceInteractor>().SetStatusTaken(holdCrossingPlaceView.ID);
-            Clean();
             UpdateInitialize();
-        }
-
-        private void OpenPanelInfo()
-        {
-
         }
 
         private void Subscribe()
@@ -111,7 +79,6 @@ namespace SlimeEvolutions.Panel.Crossing
         {
             updateView.OnDisable();
             Unsubscribe();
-            //Clean();
         }
     }
 }
