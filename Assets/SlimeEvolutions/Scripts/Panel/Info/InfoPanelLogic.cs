0f1c309dc56a20d72025f4db7f52fb5460de8ce6
KipoGo <vit-tan@mail.ru>
2023-01-21 12:04:32 +0300

    Add
    Added new class

---
+++ Assets/SlimeEvolutions/Scripts/Panel/Info/InfoPanelLogic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlimeEvolutions.Panel
+{
+    public class InfoPanelLogic
+    {
+        private InfoPanelView infoPanel;
+
+
+        public InfoPanelLogic(InfoPanelView infoPanelView)
+        {
+            infoPanel = infoPanelView;
+        }
+
+
+
+    }
+}
