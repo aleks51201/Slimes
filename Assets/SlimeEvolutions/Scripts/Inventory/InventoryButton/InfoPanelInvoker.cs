6a65370d0d65643f151246478afc156543ca0326
KipoGo <vit-tan@mail.ru>
2023-01-21 23:02:20 +0300

    Add
    Added new class

---
+++ Assets/SlimeEvolutions/Scripts/Inventory/InventoryButton/InfoPanelInvoker.cs
@@ -0,0 +1,36 @@
+﻿using SlimeEvolutions.Buttons;
+using SlimeEvolutions.InventoryCell;
+using System;
+using UnityEngine;
+
+namespace SlimeEvolutions.Inventory.InventoryButton
+{
+    public class InfoPanelInvoker:MonoBehaviour
+    {
+        [SerializeField] private ButtonWithClickAndHold button;
+
+
+        public static Action<Slime> ButtonHeldEvent;
+
+
+        private Slime GetSlime()
+        {
+            return GetComponent<CellView>().Slime;
+        }
+
+        private void OnHold()
+        {
+            ButtonHeldEvent?.Invoke(GetSlime());
+        }
+
+        private void OnEnable()
+        {
+            button.OnButtonHoldEvent += OnHold;
+        }
+
+        private void OnDisable()
+        {
+            button.OnButtonHoldEvent -= OnHold;
+        }
+    }
+}
