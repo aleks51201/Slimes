18c9e5496b075deeff6184e709e91badb8e4f2d2
KipoGo <vit-tan@mail.ru>
2023-02-04 12:46:43 +0300

    Add

---
+++ Assets/SlimeEvolutions/Scripts/Slime/Genom/Gene/Peculiarity/PeculiarityView.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace SlimeEvolutions.Slimes.Gemom
+{
+    public class PeculiarityView:MonoBehaviour
+    {
+        private void Start()
+        {
+            Peculiarity peculiarity = new();
+            peculiarity.SetSprite(this.gameObject, peculiarity.GetSprite(this.gameObject, peculiarity.GetCellSlimeId(this.gameObject)));
+        }
+    }
+}
