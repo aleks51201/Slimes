f1d990319d3bc3ba2ab27a05ddb16b079a857ff6
KipoGo <vit-tan@mail.ru>
2023-02-04 22:55:14 +0300

    Add

--- Assets/SlimeEvolutions/Scripts/Panel/Statistics/Mutagen/MutagenView.cs
+++ Assets/SlimeEvolutions/Scripts/Panel/Statistics/Mutagen/MutagenView.cs
@@ -6,5 +6,13 @@ namespace SlimeEvolutions.Panel.Statistics.Mutagen
     public class MutagenView : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI text;
+
+        private MutagenLogic mutagenLogic;
+
+
+        private void Awake()
+        {
+            mutagenLogic = new(this);
+        }
     }
 }
