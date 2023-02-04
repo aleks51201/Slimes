7348061c54b23dbf98c6913a406dac40bbc541bd
KipoGo <vit-tan@mail.ru>
2023-02-04 23:00:09 +0300

    Add

--- Assets/SlimeEvolutions/Scripts/Panel/Statistics/Mutagen/MutagenLogic.cs
+++ Assets/SlimeEvolutions/Scripts/Panel/Statistics/Mutagen/MutagenLogic.cs
@@ -1,13 +1,31 @@
-﻿namespace SlimeEvolutions.Panel.Statistics.Mutagen
+﻿using SlimeEvolutions.Architecture.Interactors.Instances;
+using SlimeEvolutions.Architecture.Scene;
+
+namespace SlimeEvolutions.Panel.Statistics.Mutagen
 {
     public class MutagenLogic
     {
         public MutagenView mutagenView;
 
 
+        private MutagenInteractor MutInteractor
+        {
+            get
+            {
+                return Game.GetInteractor<MutagenInteractor>();
+            }
+        }
+
+
         public MutagenLogic(MutagenView mutagenView)
         {
             this.mutagenView = mutagenView;
+        }
+
+
+        private void UpdateMutagenText()
+        {
+            mutagenView.Text.text = $"{MutInteractor.Mutagen}";
         }
     }
 }
