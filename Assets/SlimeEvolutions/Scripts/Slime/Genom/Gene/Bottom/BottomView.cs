d82fce965de7a3883f93b54895c88899e4fa6013
KipoGo <vit-tan@mail.ru>
2023-02-04 12:33:47 +0300

    Add

---
+++ Assets/SlimeEvolutions/Scripts/Slime/Genom/Gene/Bottom/BottomView.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace SlimeEvolutions.Slimes.Gemom
+{
+    public class BottomView : MonoBehaviour
+    {
+        private void Start()
+        {
+            Bottom bottom = new();
+            bottom.SetSprite(this.gameObject, bottom.GetSprite(this.gameObject, bottom.GetCellSlimeId(this.gameObject)));
+        }
+    }
+}
+
