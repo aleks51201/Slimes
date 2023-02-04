aa7bba0450166ff3cd6f53986dd1797fce7b9777
KipoGo <vit-tan@mail.ru>
2023-02-04 23:08:19 +0300

    Add

--- Assets/SlimeEvolutions/Scripts/Panel/Statistics/Experience/ExperienceLogic.cs
+++ Assets/SlimeEvolutions/Scripts/Panel/Statistics/Experience/ExperienceLogic.cs
@@ -1,13 +1,46 @@
-﻿namespace SlimeEvolutions.Panel.Statistics.Experience
+﻿using SlimeEvolutions.Architecture.Interactors.Instances;
+using SlimeEvolutions.Architecture.Scene;
+
+namespace SlimeEvolutions.Panel.Statistics.Experience
 {
-    public class ExperienceLogic
+    public class ExperienceLogic:IActivatable
     {
         private ExperienceView experienceView;
+
+
+        private ExperienceInteractor ExpInteractor
+        {
+            get
+            {
+                return Game.GetInteractor<ExperienceInteractor>();
+            }
+        }
 
 
         public ExperienceLogic(ExperienceView expView)
         {
             experienceView = expView;
+        }
+
+        private void UpdateMutagenText()
+        {
+            experienceView.Text.text = $"{ExpInteractor.Experience}";
+        }
+
+        private void UpdateMutagenText(int exp)
+        {
+            experienceView.Text.text = $"{exp}";
+        }
+
+        public void OnEnable()
+        {
+            UpdateMutagenText();
+            ExpInteractor.DataUpdatedEvent += UpdateMutagenText;
+        }
+
+        public void OnDisable()
+        {
+            ExpInteractor.DataUpdatedEvent += UpdateMutagenText;
         }
     }
 }
