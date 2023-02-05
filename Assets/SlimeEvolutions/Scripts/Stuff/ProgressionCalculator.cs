04f478852b2bad4a7af4c050e229d453f23df183
KipoGo <vit-tan@mail.ru>
2023-02-05 12:00:30 +0300

    Add
    Added new class

---
+++ Assets/SlimeEvolutions/Scripts/Stuff/ProgressionCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace SlimeEvolutions.Stuff
+{
+    public static class ProgressionCalculator
+    {
+        public static int CalcProgress(int start, int stop, int step)
+        {
+            int result = 0;
+            for(int i = start; i <= stop; i += step)
+            {
+                result += i * step;
+            }
+            return result;
+        }
+    }
+}
