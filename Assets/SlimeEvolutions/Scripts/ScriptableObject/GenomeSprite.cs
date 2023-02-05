c055dac0b3ce15ce39e0e0586d8478d919be076e
KipoGo <vit-tan@mail.ru>
2023-02-05 16:49:31 +0300

    Add

--- Assets/SlimeEvolutions/Scripts/ScriptableObject/GenomeSprite.cs
+++ Assets/SlimeEvolutions/Scripts/ScriptableObject/GenomeSprite.cs
@@ -7,10 +7,15 @@ public class GenomeSprite : ScriptableObject
     public Genome.Genes genes;
     public Sprite spr;
     public string name;
+    public int lvlForDrop;
+    public int weight;
+
 
     public int Id => id;
     public Genome.Genes Genes => genes;
     public Sprite Spr => spr;
     public string Name => name;
+    public int LvlForDrop => lvlForDrop;
+    public int Weight => weight;
 }
 
