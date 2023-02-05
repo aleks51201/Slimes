70c71cd63eca13c287cc2a0b70e9b3b7558ebba8
KipoGo <vit-tan@mail.ru>
2023-02-05 14:42:06 +0300

    Add

--- Assets/SlimeEvolutions/Scripts/Stuff/ProgressionCalculator.cs
+++ Assets/SlimeEvolutions/Scripts/Stuff/ProgressionCalculator.cs
@@ -2,12 +2,22 @@
 {
     public static class ProgressionCalculator
     {
-        public static int CalcProgress(int start, int stop, int step)
+        public static int CalcTotalExpForLvl(int start, int stop, int step)
         {
             int result = 0;
             for(int i = start; i <= stop; i ++)
             {
                 result += i * step;
+            }
+            return result;
+        }
+
+        public static int CalcTotalLvlForExp(int totalExp, int multiplier)
+        {
+            int result = 0;
+            for(int i = multiplier; i <= totalExp; i += multiplier)
+            {
+                result++;
             }
             return result;
         }
