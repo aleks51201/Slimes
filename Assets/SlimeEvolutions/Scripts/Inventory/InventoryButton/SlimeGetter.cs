e393f89720a2e69dd72cc8ffa80f2e2fdd6a6f27
KipoGo <vit-tan@mail.ru>
2023-02-05 16:13:34 +0300

    Refactoring

--- Assets/SlimeEvolutions/Scripts/Inventory/InventoryButton/SlimeGetter.cs
+++ Assets/SlimeEvolutions/Scripts/Inventory/InventoryButton/SlimeGetter.cs
@@ -1,15 +1,11 @@
 ﻿using SlimeEvolutions.Buttons;
 using SlimeEvolutions.InventoryCell;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
 namespace SlimeEvolutions.Inventory.InventoryButton
 {
-    public class SlimeGetter:MonoBehaviour
+    public class SlimeGetter : MonoBehaviour
     {
         [SerializeField] private ButtonWithClickAndHold button;
 
@@ -29,12 +25,12 @@ namespace SlimeEvolutions.Inventory.InventoryButton
 
         private void OnEnable()
         {
-            button.OnButtonClickEvent+= OnHold;
+            button.OnButtonClickEvent += OnHold;
         }
 
         private void OnDisable()
         {
-            button.OnButtonClickEvent-= OnHold;
+            button.OnButtonClickEvent -= OnHold;
         }
     }
 }
