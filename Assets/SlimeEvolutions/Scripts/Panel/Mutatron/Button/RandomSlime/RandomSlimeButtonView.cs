20d4a82d50966aa4f35884d1c54f402bb480d4c3
KipoGo <vit-tan@mail.ru>
2023-02-05 15:03:55 +0300

    Add

--- Assets/SlimeEvolutions/Scripts/Panel/Mutatron/Button/RandomSlime/RandomSlimeButtonView.cs
+++ Assets/SlimeEvolutions/Scripts/Panel/Mutatron/Button/RandomSlime/RandomSlimeButtonView.cs
@@ -8,12 +8,14 @@ namespace SlimeEvolutions.Panel.Mutatron
     {
         [SerializeField] private int mutagenMultiplierWhenBuying;
         [SerializeField] private ExperienceView experienceView;
+        [SerializeField] private string text;
 
         private RandomSlimeButtonLogic randomSlimeButtonLogic;
 
 
         public int MutagenMultiplier => mutagenMultiplierWhenBuying;
         public int Lvl => experienceView.Lvl;
+        public string Text => text;
 
 
         private void Start()
