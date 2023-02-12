14cac8626b4b318cbc0503e327c02f0c7f4d194d
KipoGo <vit-tan@mail.ru>
2023-02-12 20:59:48 +0300

    Fix bug
    not save resource

--- Assets/SlimeEvolutions/Scripts/Editor/GenomeSpriteEditor.cs
+++ Assets/SlimeEvolutions/Scripts/Editor/GenomeSpriteEditor.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 [CustomEditor(typeof(GenomeSprite))]
 public class GenomeSpriteEditor : Editor
 {
-    private GenomeSprite  genomeSprite;
+    private GenomeSprite genomeSprite;
     public override void OnInspectorGUI()
     {
         genomeSprite = target as GenomeSprite;
         ShowImgBlock();
         ShowBalanceBlock();
+        asdj();
     }
 
     private void ShowVerticalImgInfo()
     {
         GUILayout.BeginVertical();
-        genomeSprite.genes =(Genome.Genes) EditorGUILayout.EnumPopup("Type of part", genomeSprite.genes);
+        genomeSprite.genes = (Genome.Genes)EditorGUILayout.EnumPopup("Type of part", genomeSprite.genes);
         genomeSprite.id = EditorGUILayout.IntField(label: "Id", genomeSprite.id);
         genomeSprite.name = EditorGUILayout.TextField("Name of part", genomeSprite.name);
         GUILayout.EndVertical();
@@ -33,6 +34,17 @@ public class GenomeSpriteEditor : Editor
     {
         GUILayout.Label("BAlANCE");
         genomeSprite.lvlForDrop = EditorGUILayout.IntField("lvl for drop", genomeSprite.lvlForDrop);
-        genomeSprite.weight= EditorGUILayout.IntField("Weight of part", genomeSprite.weight);
+        genomeSprite.weight = EditorGUILayout.IntField("Weight of part", genomeSprite.weight);
+    }
+
+    private void asdj()
+    {
+        if (GUI.changed)
+        {
+            // записываем изменения над testScriptable в Undo
+            Undo.RecordObject(genomeSprite, "Test Scriptable Editor Modify");
+            // помечаем тот самый testScriptable как "грязный" и сохраняем.
+            EditorUtility.SetDirty(genomeSprite);
+        }
     }
 }
